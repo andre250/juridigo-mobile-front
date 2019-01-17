9969e711857beec0385ec54735738d531c5861fe
Grego <andrepetridis@gmail.com>
2019-01-17 01:02:32 -0200

    adding facebook login and generic login, starting maps

--- screens/App/Detail/DetailDisponiveisScreen.js
+++ screens/App/Detail/DetailDisponiveisScreen.js
@@ -1,8 +1,24 @@
 import React from 'react';
-
+import { MapView, Marker } from 'expo';
 import {  Text, View } from 'react-native';
 
 export default class DetailDisponiveisScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      markers: [
+          {
+              coordinate: {
+                  latitude: -23.519812,
+                  longitude: -46.660077
+              },
+              title: "Best Place",
+              description: "Description1",
+              id: 1
+          }
+      ]
+  };
+  }
 
   static navigationOptions = {
     title: 'JuridiGo',
@@ -17,9 +33,27 @@ export default class DetailDisponiveisScreen extends React.Component {
 
   render() {
     return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Detalhes desse item!</Text>
-      </View>
+      <MapView
+        ref={MapView => (this.MapView = MapView)}
+        style={{ flex: 1 }}
+        showsUserLocation={true}
+        initialRegion={{
+          latitude: -23.519812,
+          longitude: -46.660077,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      >
+      {this.state.markers.map((marker, i)  => (  
+              <MapView.Marker
+                key={marker.id}
+                coordinate={marker.coordinate}
+                title={marker.title}
+                description={marker.description}
+              />
+              
+      ))}
+      </MapView>
     );
   }
 }
--- screens/Auth/LoginScreen.js
+++ screens/Auth/LoginScreen.js
@@ -3,10 +3,21 @@ import {
   AsyncStorage,
   Button,
   View,
+  TextInput,
+  TouchableOpacity,
+  Text
 } from 'react-native';
 import { StyleSheet } from 'react-native';
 
 export default class LoginScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    state = {
+      us: '',
+      ps: ''
+    }
+  }
+  
   static navigationOptions = {
     title: 'Login',
   };
@@ -14,21 +25,92 @@ export default class LoginScreen extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Button title="Login!" onPress={this._signInAsync} />
+        <TextInput style = {styles.input} 
+                      autoCapitalize="none" 
+                      onSubmitEditing={() => this.passwordInput.focus()} 
+                      onChangeText={ TextInputValue =>
+                        this.setState({us: TextInputValue }) }
+                      autoCorrect={false} 
+                      keyboardType='email-address' 
+                      returnKeyType="next" 
+                      placeholder='Email ou usuário' 
+                      placeholderTextColor='rgba(225,225,225,0.7)'/>
+
+        <TextInput style = {styles.input}   
+                      returnKeyType="go" 
+                      onChangeText={ TextInputValue =>
+                        this.setState({ps: TextInputValue }) }
+                      placeholder='Senha' 
+                      placeholderTextColor='rgba(225,225,225,0.7)' 
+                      secureTextEntry/>
+
+        <TouchableOpacity style={styles.buttonContainer} 
+                            onPress={this._logInAsync}>
+                    <Text  style={styles.buttonText}>LOGIN</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.buttonContainer} 
+                            onPress={this._facebookLogInAsync}>
+                    <Text  style={styles.buttonText}>FACEBOOK</Text>
+        </TouchableOpacity>
       </View>
     );
   }
 
-  _signInAsync = async () => {
-    await AsyncStorage.setItem('userToken', 'abc');
+  _facebookLogInAsync = async () => {
+    try {
+      const {
+        type,
+        token,
+        expires,
+        permissions,
+        declinedPermissions,
+      } = await Expo.Facebook.logInWithReadPermissionsAsync('225963994956460', {
+        permissions: ['public_profile'],
+      });
+      if (type === 'success') {
+        // Get the user's name using Facebook's Graph API
+        const response = await fetch(`https://graph.facebook.com/me?access_token=${token}`);
+        await AsyncStorage.setItem('userToken', token);
+        this.props.navigation.navigate('App');
+        // console.log(`Response ${(await JSON.stringify(response))}!`);
+      } else {
+        // type === 'cancel'
+      }
+    } catch ({ message }) {
+      alert(`Facebook Login Error: ${message}`);
+    }
+  }
+  
+
+  _logInAsync = async () => {
+    await AsyncStorage.setItem('userToken', this.state.us+this.state.ps);
+    //console.log(this.state.us)
+    //console.log(this.state.ps)
     this.props.navigation.navigate('App');
   };
 }
 
 const styles = StyleSheet.create({
   container: {
+    padding: 20,
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    justifyContent: 'center'
+   },
+   input:{
+       height: 40,
+       backgroundColor: 'rgba(225,225,225,0.2)',
+       marginBottom: 10,
+       padding: 10,
+       color: '#fff'
+   },
+   buttonContainer:{
+       backgroundColor: '#2980b6',
+       paddingVertical: 15,
+       marginBottom: 10,
+   },
+   buttonText:{
+       color: '#fff',
+       textAlign: 'center',
+       fontWeight: '700'
+   }
 });
