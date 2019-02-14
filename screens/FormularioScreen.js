a5ee794009e522041b3cc52be095fde6b2b81adb
Grego <andrepetridis@gmail.com>
2019-02-13 22:34:53 -0200

    Adding forms component elements

--- screens/FormularioScreen.js
+++ screens/FormularioScreen.js
@@ -4,6 +4,9 @@ import { LogoTitle } from '../components/LogoTitle';
 import Wizard from '../components/Wizard';
 import Input from '../components/Input';
 import CadastralForm from '../components/Forms/CadastralForm';
+import DocumentForm from '../components/Forms/DocumentForm';
+import EducationForm from '../components/Forms/EducationForm';
+import PaymentForm from '../components/Forms/PaymentForm';
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
 
 export default class FormularioScreen extends React.Component {
@@ -20,7 +23,7 @@ export default class FormularioScreen extends React.Component {
   render() {
     return (
       <View style={styles.root}>
-          <CadastralForm
+          <PaymentForm
             // onChangeValue={onChangeValue}
             // placeholder={el.placeholder}
             // value={values[el.name]}
